4adebce5a1e5471f3f728182f21d15f3c53ff182
Patrinysilva <patrinysilva17@gmail.com>
2025-03-27 17:33:29 -0300

    Configurando o uglify

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,13 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+
+function scripts(){
+    return gulp.src('./src/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js/'))
+}
 
 // 
 function styles(){
@@ -18,9 +25,10 @@ function images(){
     // Essa funcion vai minificar as imagens
 }
 // define tarefas a serem executadas
-exports.default = gulp.parallel(styles, images);
+exports.default = gulp.parallel(styles, images, scripts);
 
 exports.watch = function(){
     gulp.watch('./src/styles/*.scss',gulp.parallel(styles))
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
 }
 // para rodar outra vez use npm run build watch
